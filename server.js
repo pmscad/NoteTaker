a59b5e05dcf12cba5071962ef56f24182de6813d
vmultani <vmultani@blueyonder.co.uk>
2019-12-15 10:12:24 +0000

    HTML routes succesfully tested

--- server.js
+++ server.js
@@ -5,20 +5,37 @@ const path = require("path");
 // need fs to read and write to files
 const fs = require("fs");
 
-// Initialize the app and create a port
-
+// creating an "express" server
 const app = express();
-const PORT = process.env.PORT || 3000;
+// Sets an Initial port for listeners
+const PORT = process.env.PORT || 8080;
+
+//  Initialize notesData
+
+let notesData = [];
 
 // Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+app.use(express.static(path.join(__dirname, "public")));
+
+// routes
+
+// api call response for all the notes, and sends the results to the browser as an array of object
+
+// HTML GET Requests
+
+// Web page when the Get started button is clicked
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// If no matching route is found default to home
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 // Start the server on the port
-
 app.listen(PORT, function() {
   console.log("SERVER IS LISTENING: " + PORT);
 });
